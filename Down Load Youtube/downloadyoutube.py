563cd2bf6aa74277eaa549aa4a7bcffc7e56175f
Tinh <tinh.nguyentrung311@gmail.com>
2021-05-01 21:01:53 +0700

    Tree - C++

--- Down Load Youtube/downloadyoutube.py
+++
@@ -1,5 +0,0 @@
-from pytube import YouTube
-url = "https://www.youtube.com/watch?v=bTFoZBIIu4E"
-yt = YouTube(url).streams.filter(progressive=True,file_extension='mp4').first().download()
-
-print(yt)
